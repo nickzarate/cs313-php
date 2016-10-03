0845b9b472955652dcba306e7a10803ccb310697
Nick <nicholas.zarate@gmail.com>
2016-10-03 09:07:19 -0600

    Test slider

--- web/assignments/03/survey.js
+++ web/assignments/03/survey.js
@@ -1,3 +1,16 @@
 function submitForm() {
   // TODO: create the function
 }
+
+function setup() {
+  // Instantiate a slider
+  var mySlider = $("input.slider").slider();
+
+  // Call a method on the slider
+  var value = mySlider.slider('getValue');
+
+  // For non-getter methods, you can chain together commands
+  mySlider
+    .slider('setValue', 5)
+    .slider('setValue', 7);
+}
